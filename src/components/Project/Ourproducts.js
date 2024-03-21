26b84bd05bedff774022031d1b5c33f5fe78b2b0
183hardik <hardik_c@ce.iitr.ac.in>
2024-03-21 22:40:42 +0530

    ark

---
+++ src/components/Project/Ourproducts.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "D:/git/Enactus-website/src/components/Project/Ourproducts.css"
+
+function Ourproducts() {
+  return (
+    <div className='prodwrapper'>
+      <h1>Our products</h1>
+    </div>
+  )
+}
+
+export default Ourproducts
---
+++ src/components/Project/ourdata.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "D:/git/Enactus-website/src/components/Project/ourprogress.css"
+import progressimage from "D:/git/Enactus-website/src/Assets/Group 626387.png"
+
+function Progr() {
+  return (
+    <div className='wrapper'>
+     <h1>OUR PROGRESS</h1>
+     <img src={progressimage}></img>
+    </div>
+  )
+}
+
+export default Progr;
